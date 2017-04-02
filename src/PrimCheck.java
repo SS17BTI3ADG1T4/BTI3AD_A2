cd2db405c9033d58ec5bba2945b1a7911c38cbb6
Sönke Peters <peters.soenke@googlemail.com>
2017-04-02 11:04:19 +0200

    Raw Algorithms
    Added the Algorithms fast prim needs some speed

---
+++ src/PrimCheck.java
@@ -0,0 +1,44 @@
+
+public class PrimCheck {
+  
+  private Counter counter;
+  
+  
+  
+  public PrimCheck() {
+  }
+  
+  public PrimCheck(Counter counter) {
+    this();
+    this.counter = counter;
+  }
+
+
+  
+  public boolean isPrime(int num) {
+    if (num % 2 == 0){
+      count();
+      return false;
+     
+    }
+    for (int i = 3; i * i < num; i += 2){
+      count();  
+      if (num % i == 0){
+        count();
+        return false;
+      }
+    }
+    return true;
+}
+  
+  
+  
+  /**
+   * Count.
+   */
+  private void count() {
+        if(counter != null){
+            counter.counterUp(1);
+        }
+    }
+}
---
+++ src/SlowPrim.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class SlowPrim {
+  
+  private Counter counter;
+  
+  
+ public SlowPrim() {
+  }
+ 
+ public SlowPrim(Counter counter) {
+   this();
+   this.counter = counter;
+ }
+
+
+public ArrayList<Integer> primzahlen(int N) {
+    boolean a[] = new boolean[N];
+    count();
+    for ( int i = 0; i < N ; i++){
+      a[i] = true;
+      count();
+    }
+    for ( int i = 2; i < N; i++) {
+      count();
+      for ( int j = 2; j < N; j++){
+        count();
+        if ( (i%j == 0) && (j!=i) ){
+          a[i] = false;
+          count();
+        }
+      }
+      }
+    ArrayList<Integer> list = new ArrayList<>();
+    for(int p =0; p<a.length;p++){
+      count();
+      if(a[p]){
+        list.add(p);
+        count();
+      }
+    }
+    return list;
+    
+    }
+
+
+
+/**
+ * Count.
+ */
+private void count() {
+      if(counter != null){
+          counter.counterUp(1);
+      }
+  }
+
+
+}
